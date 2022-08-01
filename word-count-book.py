bf250f0d6cbdb90025204364ffde1cba642e217b
aexye <s25754@pjwstk.edu.pl>
2022-08-01 22:14:04 +0200

    added first commit

--- word-count-book.py
+++ word-count-book.py
@@ -1,18 +1,17 @@
 from pyspark import SparkConf, SparkContext
 
-# oryginalna nazwa skryptu: book_words_count.py
 
 conf = SparkConf().setMaster("local").setAppName("BookWordsCount")
 sc = SparkContext(conf=conf)
 
-lines = sc.textFile("pg20417.txt")
+line = sc.textFile("pg20417.txt")
 
-words = lines.flatMap(lambda x: x.split(" "))
+words = line.flatMap(lambda x: x.split(" "))
 pairs = words.map(lambda x: (x, 1))
 wordCount = pairs.reduceByKey(lambda a, b: a + b)
 
-results = wordCount.collect()
 
-for result in results:
-    # if result[1] > 5: # dla przejrzystości wyników
-    print("{}\t{}".format(result[0], result[1]))
+counts = wordCount.collect()
+
+for count in counts:
+    print("{}\t{}".format(count[0], count[1]))
